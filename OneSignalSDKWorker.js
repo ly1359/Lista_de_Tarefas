3719963caff9d960246382f4b516ad7404ea68ef
ly1359 <diellylopes330@gmail.com>
2023-05-26 09:32:46 -0300

    ajuste

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,4 +1,1 @@
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
-OneSignal.SERVICE_WORKER_PATH = 'Lista_de_Tarefas/OneSignalSDKWorker.js';
-OneSignal.SERVICE_WORKER_UPDATER_PATH = 'Lista_de_Tarefas/OneSignalSDKUpdaterWorker.js';
-OneSignal.init();
